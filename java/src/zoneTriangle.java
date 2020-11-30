748ed0fbfd215f1e771f043779ed176c51c89ea6
[AK-47-SOIJ] <jioska_one@outlook.es>
2020-11-29 21:22:15 -0500

    add zone triangle

--- java/src/zoneTriangle.java
+++ java/src/zoneTriangle.java
@@ -1,5 +1,12 @@
+//cacular el area de un triangulo por su base y altura
 public class zoneTriangle {
     public static void main(String[] args) {
-        
+        var base = 5;
+        var  height = 4;
+
+        System.out.println(calculateZona(base,height));
+    }
+    public static double calculateZona(int b, int h){
+        return Math.floorDiv((b*h),2);
     }
 }
