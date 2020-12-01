c4a18f8d933781f83c1aaa09a16b5f4e62fd0a2d
[AK-47-SOIJ] <jioska_one@outlook.es>
2020-11-30 19:07:57 -0500

    add docs

--- java/src/CostOfItems.java
+++ java/src/CostOfItems.java
@@ -1,7 +1,14 @@
+
 //El costo mensual C en dolares al fabricar una cantidad x de articulos esta dado por:
 //C = 50 + 2x, mientras que el ingreso por la venta de X articulos esta dada por V = 2.4x
 //a) Calcule la ganacia que se obtendra al fabricar y vender 400 articulos
 //b) Determine cuantos articulos deben fabricarse y venderse para que el ingreso iguale a los gastos.
+/**
+ * El costo mensual C en dolares al fabricar una cantidad x de articulos esta dado por:
+ * C = 50 + 2x, mientras que el ingreso por la venta de X articulos esta dada por V = 2.4x
+ * a) Calcule la ganacia que se obtendra al fabricar y vender 400 articulos
+ * b) Determine cuantos articulos deben fabricarse y venderse para que el ingreso iguale a los gastos.
+ * */
 public class CostOfItems {
     public static void main(String[] args) {
         var madeArticles = 400;
@@ -16,16 +23,26 @@ public class CostOfItems {
         System.out.println("gastado al mes: "+createdArticules(direct));
         System.out.println("total de 125 articulos: "+total(direct));
     }
-    //aqui se calcula el total de la ganancia o la perdida
+    /**
+     * @param x Representa el total de ganancias obtenidas en gastos y ventas
+     * @return Devuelve una funcion el cual calcula el dinero obtenido entre ventas y gastos
+     * */
     public static double total(int x){
         return (2.4 * x) - (50 + 2 * x) ;
     }
-    //aqui solo se calcula la ganancia
+    /**
+     * @param v representa solo las ventas
+     * @return devuelve una funcion donde solo representan las ganancias en las ventas
+     * */
     public static double earningsArticles(int v){
         return 2.4 * v;
     }
-    //aqui solo se calcula lo gastado
+    /**
+     * @param c Representa dinero gastado en crear material para vender
+     * @return representa el dinero gastado en materiales
+     * */
     public static double createdArticules(int c){
         return 50 + 2 * c;
+
     }
 }
