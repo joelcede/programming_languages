35b8fecf757cd84609b2f06b503c5c22edba260f
[AK-47-SOIJ] <jioska_one@outlook.es>
2020-12-03 17:40:28 -0500

    add definicion

---
+++ java se11/src/DefinitionXd.java
@@ -0,0 +1,24 @@
+/**
+ * Escribe un programa que muestre por pantalla 10 palabras en inglés junto
+ * a su correspondiente traducción al castellano. Las palabras deben estar
+ * distribuidas en dos columnas y alineadas a la izquierda. Pista: Se puede
+ * insertar un tabulador mediante \t
+ */
+public class DefinitionXd {
+    public static void main(String[] args) {
+        String color = "\033[31m";
+        String color2 = "\033[37m";
+        //idk xd
+        System.out.println(color+"cat :"+color2+"\tgato");
+        System.out.println(color+"dog :"+color2+"\tperro");
+        System.out.println(color+"paper :"+color2+"\tpapel");
+        System.out.println(color+"hi :"+color2+"\thola");
+        System.out.println(color+"god :"+color2+"\tdios");
+        System.out.println(color+"computer :"+color2+"\tcomputadora");
+        System.out.println(color+"robot :"+color2+"\trobot");
+        System.out.println(color+"door :"+color2+"\tpuerta");
+        System.out.println(color+"windows :"+color2+"\tventanas");
+        System.out.println(color+"table :"+color2+"\tmesa");
+
+    }
+}
