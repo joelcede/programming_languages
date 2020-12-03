f47002ead159beb73b74fa8af177454af1715dc5
[AK-47-SOIJ] <jioska_one@outlook.es>
2020-12-03 18:29:39 -0500

    add piramide

---
+++ java se11/src/Pyramid.java
@@ -0,0 +1,14 @@
+/**
+ * Escribe un programa que pinte por pantalla una pirámide rellena a base de
+ * asteriscos. La base de la pirámide debe estar formada por 9 asteriscos.
+ */
+public class Pyramid {
+    public static void main(String[] args) {
+        String color = "\033[033m";
+        System.out.println(color+"\t*");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+    }
+}
