30047e5f4b40a38886ea657392c947d1060f9748
[AK-47-SOIJ] <jioska_one@outlook.es>
2020-12-03 19:43:03 -0500

    add avicii arma

---
+++ java se11/src/Avicii.java
@@ -0,0 +1,20 @@
+/**
+ * Escribe un programa que pinte por pantalla alguna escena - el campo, la
+ * habitación de una casa, un aula, etc. - o algún objeto animado o inanimado
+ * - un coche, un gato, una taza de café, etc. Ten en cuenta que puedes utilizar
+ * caracteres como *, +, <, #, @, etc. ¡Échale imaginación!
+ */
+public class Avicii {
+    public static void main(String[] args) {
+        System.out.println(" ________");
+        System.out.println(" \\HHHHHH|");
+        System.out.println("  \\HHHHHH|        ____        ____    _____+---+-------\\");
+        System.out.println("  H\\HHHHHH\\------/____\\_______/___\\__/_________|       |--+");
+        System.out.println("  H|HHHHHHHH|    [ |     |             ________|       |  |");
+        System.out.println("  H|HHHHHHHH|    [_|     |             |       |       |--+");
+        System.out.println("  H/HHHHHHH/--+----+-----+----+ññ+---ñññ+------\\_______/  ");
+        System.out.println("  /HHHHHH/    |    |     /    |ÑÑ|  |ÑÑÑ|==]");
+        System.out.println(" /HHHHH/      |    |    |___--+ÑÑ+--|ÑÑÑ/");
+        System.out.println("              +----+");
+    }
+}
