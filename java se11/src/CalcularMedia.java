8a906f394f80badb68336cfbb3904614b80d9d0c
[AK-47-SOIJ] <jioska_one@outlook.es>
2020-12-07 17:33:56 -0500

    version 2

--- java se11/src/CalcularMedia.java
+++ java se11/src/CalcularMedia.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 /**
- * Realiza un programa que calcule la media de tres notas.
+ * Amplía el programa anterior para que diga la nota del boletín (insuficiente,
+ * suficiente, bien, notable o sobresaliente).
  */
 public class CalcularMedia {
     public static void main(String[] args) {
@@ -16,7 +17,19 @@ public class CalcularMedia {
 
         var resultadoSuma = suma(primero,segundo,tercero);
         var resultadoFinal = dividir(resultadoSuma);
-        System.out.println("la media es: "+resultadoFinal);
+        System.out.println("la nota es: "+resultadoFinal);
+
+        if (resultadoFinal < 4){
+            System.out.println("tiene una nota insuficiente");
+        }if ((resultadoFinal >=5) && (resultadoFinal <= 6)){
+            System.out.println("tiene una nota suficiente");
+        }if (resultadoFinal == 7){
+            System.out.println("tiene buena nota");
+        }if (resultadoFinal == 8){
+            System.out.println("Su nota es notable");
+        }if (resultadoFinal >= 9){
+            System.out.println("Su nota es sobresaliente");
+        }
     }
     public static int suma(int primero, int segundo, int tercero){
         return primero + segundo + tercero;
