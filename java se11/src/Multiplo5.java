b6dd697c5f78c01489455d391596388335ea359d
[AK-47-SOIJ] <jioska_one@outlook.es>
2020-12-16 13:09:11 -0500

    v3 con do while

--- java se11/src/Multiplo5.java
+++ java se11/src/Multiplo5.java
@@ -1,12 +1,12 @@
-//Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+//Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.
 public class Multiplo5 {
     public static void main(String[] args) {
         int numero = 0;
-        while (numero<100){
-            if (numero% 5 == 0){
+        do {
+            numero++;
+            if (numero%5 == 0){
                 System.out.println(numero);
             }
-            numero++;
-        }
+        }while (numero < 101);
     }
 }
