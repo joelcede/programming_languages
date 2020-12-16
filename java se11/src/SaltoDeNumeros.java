bb348942b0d6acb806dbe508badb2bd99607a7e1
[AK-47-SOIJ] <jioska_one@outlook.es>
2020-12-16 13:27:29 -0500

    add v3 con do while

--- java se11/src/SaltoDeNumeros.java
+++ java se11/src/SaltoDeNumeros.java
@@ -1,12 +1,13 @@
-//Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás
-//utilizando un bucle while.
+//Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle
+//do-while.
 public class SaltoDeNumeros {
     public static void main(String[] args) {
         int numeroSuperior = 340;
         int numeroInferior = 169;
-        while (numeroSuperior > numeroInferior){
-            numeroSuperior = numeroSuperior - 20;
+
+        do {
+            numeroSuperior = numeroSuperior -20;
             System.out.println(numeroSuperior);
-        }
+        }while (numeroSuperior > numeroInferior);
     }
 }
