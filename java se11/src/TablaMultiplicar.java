291f2b30f1ada1a2850c0650cc9e4f0d5336aa3e
[AK-47-SOIJ] <jioska_one@outlook.es>
2020-12-16 13:41:54 -0500

    add tabla de multiplicar con ciclo for

---
+++ java se11/src/TablaMultiplicar.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//Muestra la tabla de multiplicar de un número introducido por teclado.
+
+public class TablaMultiplicar {
+    public static void main(String[] args) {
+        Scanner numero = new Scanner(System.in);
+
+        System.out.println("Tabla hasta del 1 al 10");
+        System.out.print("Ingrese un numero: ");
+        int numeroTabla = numero.nextInt();
+
+        for (int i = 0; i < 11; i++) {
+            System.out.println(numeroTabla+" * "+i+" = "+(numeroTabla*i));
+        }
+    }
+}
