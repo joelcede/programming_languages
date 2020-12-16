0a1aaec9a615e347ea71374054afeff078b79c35
[AK-47-SOIJ] <jioska_one@outlook.es>
2020-12-16 13:17:11 -0500

    add cuenta regresiva

---
+++ java se11/src/SaltoDeNumeros.java
@@ -0,0 +1,10 @@
+//Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás
+//utilizando un bucle for.
+public class SaltoDeNumeros {
+    public static void main(String[] args) {
+        int numero = 159;
+        for (int i = 320; i >numero; i=i-20) {
+            System.out.println(i);
+        }
+    }
+}
