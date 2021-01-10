298db8ff46ef45d1ffe13fbb83d32d9de778797f
[AK-47-SOIJ] <jioska_one@outlook.es>
2021-01-10 17:01:25 -0500

    add 9

---
+++ javascript/9.js
@@ -0,0 +1,11 @@
+
+// operadores
+let numero = 2;
+let numero2 = 3;
+let txto = "2"
+document.writeln(numero == numero2);
+document.writeln(numero === txto);
+document.writeln(numero != numero2);
+document.writeln(numero !== numero2);
+document.writeln(numero <= numero2);
+document.writeln(numero >= numero2);
