2091d1c873e611ca1f69816883c788edea977e5e
[AK-47-SOIJ] <jioska_one@outlook.es>
2021-01-10 17:07:48 -0500

    add 10

---
+++ javascript/10.js
@@ -0,0 +1,15 @@
+// let valor1 = true;
+// let valor3 = true;
+
+// let resultado = valor && valor2;
+// let resultado2 = valor || valor2;
+// let resultado3 = !valor;
+
+num1 = 12;
+num2 = 13;
+
+afirmacion1 = num1 < num2;
+afirmacion2 = num1 != num2;
+
+document.writeln(afirmacion1 || afirmacion2)
+document.writeln(!afirmacion1)
