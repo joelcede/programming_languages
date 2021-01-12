711012d7c0a0691ac684a959e510aef6b6fc4fe9
[AK-47-SOIJ] <jioska_one@outlook.es>
2021-01-12 16:37:23 -0500

    fig

---
+++ Lenguaje C/figures.c
@@ -0,0 +1,19 @@
+/*
+Escriba un programa que imprima una caja, un óvalo, 
+una flecha y un diamante como los siguientes:
+*/
+
+#include<stdio.h>
+
+int main(){
+	printf("*********    ***      *         *\n");
+	printf("*       *   *   *    ***       * *\n");
+	printf("*       *  *     *  *****     *   *\n");
+	printf("*       *  *     *    *      *     *\n");
+	printf("*       *  *     *    *     *       *\n");
+	printf("*       *  *     *    *      *     *\n");
+	printf("*       *  *     *    *       *   *\n");
+	printf("*       *   *   *     *        * *\n");
+	printf("*********    ***      *         *\n");
+	return 0;
+}
