752b7e0ac8de3847a95c39a115e1cf68a1c620cb
[AK-47-SOIJ] <jioska_one@outlook.es>
2021-01-13 11:06:32 -0500

    add add

---
+++ Lenguaje C/fg.c
@@ -0,0 +1,22 @@
+/*
+Despliegue el siguiente patrón de diseño mediante ocho instrucciones printf, 
+y después despliegue el mismo patrón con el menor número posible de instrucciones printf.
+*/
+
+#include<stdio.h>
+
+int main(){
+	printf("* * * * * * * *\n");
+	printf(" * * * * * * * *\n");
+	printf("* * * * * * * *\n");
+	printf(" * * * * * * * *\n");
+	printf("* * * * * * * *\n");
+	printf(" * * * * * * * *\n");
+	printf("* * * * * * * *\n");
+	printf(" * * * * * * * *\n");
+	
+	printf("\n");
+	printf("* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n  * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n * * * * * * * *\n  * * * * * * * *\n");
+
+	return 0;
+}
