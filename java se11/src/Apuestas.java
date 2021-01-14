2d1a11302d1335c7d820e7f7758dc2c009b7ec7d
[AK-47-SOIJ] <jioska_one@outlook.es>
2021-01-13 21:56:50 -0500

    add pendiente

---
+++ java se11/src/Apuestas.java
@@ -0,0 +1,9 @@
+/**
+ * Escribe un programa que muestre tres apuestas de la quiniela en tres columnas
+ * para los 14 partidos y el pleno al quince (15 filas).
+ */
+public class Apuestas {
+    public static void main(String[] args) {
+        System.out.println("no entendi ninguna palabra de lo que dice aqui luego investigo");
+    }
+}
