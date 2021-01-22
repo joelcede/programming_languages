c7c07a31968f320751fc92edfa4542535295c9f6
[AK-47-SOIJ] <jioska_one@outlook.es>
2021-01-21 21:53:48 -0500

    add inverso

--- java se11/src/OrdenParArray.java
+++ java se11/src/OrdenParArray.java
@@ -1,3 +1,10 @@
+/*
+Escribe un programa que genere 20 números enteros aleatorios entre 0 y 100
+y que los almacene en un array. El programa debe ser capaz de pasar todos
+los números pares a las primeras posiciones del array (del 0 en adelante) y
+todos los números impares a las celdas restantes. Utiliza arrays auxiliares si
+es necesario
+ */
 public class OrdenParArray {
     public static void main (String[] args){
         int[] numero1 = new int[20];
