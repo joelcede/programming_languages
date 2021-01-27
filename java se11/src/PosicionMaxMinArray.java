d65221c81dd6d76b48e7ea832e60d884c8095e31
[AK-47-SOIJ] <jioska_one@outlook.es>
2021-01-27 18:48:22 -0500

    adfgsg

--- java se11/src/PosicionMaxMinArray.java
+++ java se11/src/PosicionMaxMinArray.java
@@ -1,10 +1,30 @@
 public class PosicionMaxMinArray {
     public static void main(String[] args) throws InterruptedException {
-
         int[][] num = new int[6][10];
 
         int fila;
         int columna;
+
+        // Genera el contenido del array sin que se repita ningún valor
+        boolean repetido;
+        int i;
+        int j;
+
+        for(fila = 0; fila < 6; fila++) {
+            for(columna = 0; columna < 10; columna++) {
+                do {
+                    num[fila][columna] = (int)(Math.random() * 1001);
+
+                    // Comprueba si el número generado ya está en el array.
+                    repetido = false;
+                    for (i = 0; i < 10 * fila + columna; i++) {
+                        if (num[fila][columna] == num[i / 10][i % 10]) {
+                            repetido = true;
+                        }
+                    }
+                } while (repetido);
+            }
+        }
 
         int minimo = Integer.MAX_VALUE;
         int filaMinimo = 0;
@@ -29,7 +49,6 @@ public class PosicionMaxMinArray {
         for(fila = 0; fila < 6; fila++) {
             System.out.print("  " + fila + " │");
             for(columna = 0; columna < 10; columna++) {
-                num[fila][columna] = (int)(Math.random() * 1001);
                 System.out.printf("%5d ", num[fila][columna]);
                 Thread.sleep(100);
 
